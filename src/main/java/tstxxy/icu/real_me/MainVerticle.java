a5e8c8f1e80620519a37989c4e991b3037682aff
tstxxy <tstxxy@gmail.com>
2020-11-16 16:23:43 +0800

    change deploy

--- src/main/java/tstxxy/icu/real_me/MainVerticle.java
+++ src/main/java/tstxxy/icu/real_me/MainVerticle.java
@@ -11,16 +11,20 @@ public class MainVerticle extends AbstractVerticle {
     public void start(Promise<Void> startPromise) throws Exception {
         router = Router.router(vertx);
 
-        router.route("/hello").handler(req -> {
-            req.response()
-                    .putHeader("content-type", "text/plain")
-                    .end("Hello from Yuan Haomin!");
+        router.get("/hello").handler(ctx -> {
+            var response = ctx.response();
+            response.setChunked(true);
+            response.putHeader("content-type", "text/plain");
+            ctx.vertx().setTimer(1000, tid -> ctx.next());
+        }).handler(ctx -> {
+            var response = ctx.response();
+            response.write("Hello from Yuan Haomin").end();
         });
 
-        vertx.createHttpServer().requestHandler(router).listen(8888, http -> {
+        vertx.createHttpServer().requestHandler(router).listen(80, http -> {
             if (http.succeeded()) {
                 startPromise.complete();
-                System.out.println("HTTP server started on port 8888");
+                System.out.println("HTTP server started on port 80");
             } else {
                 startPromise.fail(http.cause());
             }
